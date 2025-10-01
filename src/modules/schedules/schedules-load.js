f9ccf8db77a7c5e9c859a1399fe69924b39e9555
Arthur Oliveira <zallarthur@gmail.com>
2025-10-01 18:15:37 -0300

    feat:  cria método para exibir agendamentos dinamicamente ná página

--- src/modules/schedules/schedules-load.js
+++ src/modules/schedules/schedules-load.js
@@ -1,13 +1,15 @@
 import { hoursLoad } from "../form/hours-load"
 import { scheduleFetchByDay } from "../../service/schedule-fetch-by-day.js" 
+import { scheduleShow } from "../schedules/show.js"
 
 const selectedDate = document.getElementById("date")
 
 export async function schedulesDay() {
     const date = selectedDate.value;
 
-    const dailySchedules = await scheduleFetchByDay( {date} )
-    console.log(dailySchedules);
+    const dailySchedules = await scheduleFetchByDay( {date} );
+
+    scheduleShow( { dailySchedules } );
 
     hoursLoad({ date });
 }
